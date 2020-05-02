88b049af39cb035cf4c9dd4ac83ec5acbbfa1d6d
Ayanna Shaheed <ayanna.shaheed@gmail.com>
2020-05-02 19:44:46 -0400

    Template for readme laidout

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,46 @@
 function generateMarkdown(data) {
   return `
-# ${data.title}
+  [![made-with-Markdown](https://img.shields.io/badge/Made%20with-Markdown-1f425f.svg)](http://commonmark.org)
+
+  # ${data.title}
+  
+  ${data.description}
+  
+  ## Table of Contents
+  
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [License](#license)
+  4. [Contributing](#contributing)
+  5. [Tests](#tests)
+  6. [Questions](#questions)
+  
+  ## Installation
+  
+  The application will be installed with the following command(s):
+  \`\`\`sh
+  ${data.installationCommands}
+  \`\`\`
+  ## Usage
+  
+  ${data.usageInstrutions}
+  \`\`\`sh
+  ${data.usageCommands}
+  \`\`\`
+  ## Contributing
+  
+  ${data.contributing}
+  
+  ## Tests
+  
+  ${data.tests}
+  
+  ## Questions
+  Have a question? Shoot me an email
+  
+  Email: [$data.email](mailto:${data.email})
+  
+  ![Image of User](${data.avatar})
 
 `;
 }
